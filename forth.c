5ce1b7c4a85883b58025cdb7b1ba3d0ab89d3a52
Martin Gergov <martingergov1@gmail.com>
2018-03-11 14:09:05 +0100

    some cleaning, some license

--- forth.c
+++ forth.c
@@ -1,7 +1,3 @@
-
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-
 extern void PUT32 ( unsigned int, unsigned int );
 extern unsigned int GET32 ( unsigned int );
 extern void dummy ( unsigned int );
@@ -40,7 +36,7 @@ void uart_putc ( unsigned int c )
     }
     PUT32(AUX_MU_IO_REG,c);
 }
-
+//------------------------------------------------------------------------
 void uart_puts ( char* str )
 {
     unsigned int rb = 0;
@@ -112,10 +108,6 @@ int notmain ( unsigned int earlypc )
             if(GET32(AUX_MU_LSR_REG)&0x01) break;
         }
         ra=GET32(AUX_MU_IO_REG);
-        //while(1)
-        //{
-            //if(GET32(AUX_MU_LSR_REG)&0x20) break;
-        //}
         /* PUT32(AUX_MU_IO_REG,ra); */
 	/* uart_putc(ra); */
 	/* uart_putc(0x20); */
@@ -124,18 +116,3 @@ int notmain ( unsigned int earlypc )
 
     return(0);
 }
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-
-
-//-------------------------------------------------------------------------
-//
-// Copyright (c) 2012 David Welch dwelch@dwelch.com
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//-------------------------------------------------------------------------
